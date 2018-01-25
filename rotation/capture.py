b82602defc1d53d9c5de18e5cc9f7aed804bf13c
M Chadwick <dr.m.chadwick@gmail.com>
2018-01-25 17:11:50 +0000

    Added some image comparison algorithms from scikit.

--- rotation/capture.py
+++ rotation/capture.py
@@ -8,6 +8,7 @@ import pyautogui
 import os
 
 from mouse import mousemove
+from skimage.measure import compare_ssim, compare_mse, compare_psnr
 
 #monitor = {'top': 14, 'left': 0, 'width': 800, 'height': 600}
 # time.sleep(2)
@@ -41,14 +42,14 @@ from mouse import mousemove
 #         #time.sleep(2)
 
 
-def buildVertPanorama(step=100, delay=10):
+def buildVertPanorama(step=375, delay=10):
     time.sleep(delay)
     with mss.mss() as capture:
         monitor = capture.monitors[1]
         imgArray = [numpy.array(capture.grab(monitor))]
 
-        for _ in range(100):
-            mousemove(movex=step, movey=0, delay=0.2)
+        for _ in range(20):
+            mousemove(movex=step, movey=0, delay=0)
             imgArray.append(numpy.array(capture.grab(monitor)))
     
     # stitcher = cv2.createStitcher(False)
@@ -57,10 +58,15 @@ def buildVertPanorama(step=100, delay=10):
     # cv2.imshow('Verticle Stitch test', result)
     # if cv2.waitKey(25) & 0xFF == ord('q'):
     #         cv2.destroyAllWindows()
+    orig = imgArray[0]
     for i in imgArray:
-        cv2.imshow('Verticle Stitch test', i)
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
+        print("MSE: ", compare_mse(orig, i))
+        print("ssim: ", compare_ssim(orig, i, multichannel=True))
+        print("psnr: ", compare_psnr(orig, i))
+        # cv2.imshow('Verticle Stitch test', i)
+        # cv2.waitKey(0)
+        # cv2.destroyAllWindows()
 
     return imgArray
 
+#def mean_square_error()
